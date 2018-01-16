3e590cad288273855a745b94d4b2b38a09459219
github username <github email address>
2018-01-16 16:01:19 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,13 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    # @newpost = Post.new(post_params)
+    if @post.update(post_params)
+      # above will implicitly run a validation 
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
